c00fdd47ac7927b7e44e204b77baf7435d3a65c9
TAT-PRINCE <ypr.nroclk@ynadex.ru>
2024-03-17 23:30:42 +0300

    ABOBUS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -59,10 +59,10 @@ DATABASES = {
         # Меняем настройку Django: теперь для работы будет использоваться
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'NAME': os.getenv('DB_NAME', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('POSTGRES_DB', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
